1bc9cfcb6a38eae7a472b006959da09ef1e6c5f0
Miranda Lupion <lupion@g.harvard.edu>
2018-12-28 11:17:28 -0500

    delete

--- data_prep.R
+++
@@ -1,8 +0,0 @@
-#Data prep
-library(tidyverse)
-library(readr)
-library(reshape2)
-
-area_by_district <- read_csv("area_by_district_1908.csv")
-
-tidy <- gather(area_by_district, key = "condition", value, -district)
