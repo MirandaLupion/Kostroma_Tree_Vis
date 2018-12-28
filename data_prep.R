43e3dc83339836e28f4276372e2bc6edc9a1bcb6
Miranda Lupion <lupion@g.harvard.edu>
2018-12-28 11:16:51 -0500

    cleaning

--- data_prep.R
+++ data_prep.R
@@ -1,9 +1,8 @@
 #Data prep
 library(tidyverse)
 library(readr)
+library(reshape2)
 
 area_by_district <- read_csv("area_by_district_1908.csv")
 
-area_change <- read_csv("area_change_1818_1908.csv")
-
-species <- read_csv("species.csv")
+tidy <- gather(area_by_district, key = "condition", value, -district)
--- kostroma_area_app/area_app.R
+++ kostroma_area_app/area_app.R
@@ -28,12 +28,18 @@ ui <- fluidPage(fluidPage(theme = shinytheme("united")),
                 
  sidebarLayout(
    sidebarPanel(
-     selectizeInput(inputId = "y", #internal label 
-                 label = "Select measure to add to the bar graph", #label that user sees
-                 choices = area_options, #vector of choices for user to pick from 
-                 multiple = TRUE,
-                 selected = area_options[1],
-                 options = list(maxItems = 5)),
+     
+     radioButtons("radio", 
+                  label = "View data as",
+                  choices = list("percent" = 1, "raw" = 2), 
+                  selected = 1),
+     
+     # selectizeInput(inputId = "y", #internal label 
+     #             label = "Select measure to add to the bar graph", #label that user sees
+     #             choices = area_options, #vector of choices for user to pick from 
+     #             multiple = TRUE,
+     #             selected = area_options[1],
+     #             options = list(maxItems = 5)),
      
      selectizeInput(inputId = "d", #internal label
                     label = "Select districts to include", #label that user sees
@@ -52,14 +58,19 @@ ui <- fluidPage(fluidPage(theme = shinytheme("united")),
 server <- function(input, output) {
   
   pop_subset <- reactive({
-    req(input$d, input$y)
-    filter(area_by_district, district %in% input$d & condition %in% input$y) })
+    req(input$d, input$radio)
+    if (input$radio == 1) {
+      filter(area_by_district, district %in% input$d & condition %in% c("percent_forested_suitable", "percent_forested_total"))
+    } else {
+      filter(area_by_district, district %in% input$d & condition %in% c("total_area_desyatin", "suitable_area_desyatin", "forested_area_desyatin"))
+    }
+    })
   
   output$plot <- renderPlot({
     ggplot(data = pop_subset(), aes_string(x = "district", y = "value", fill = "condition")) + 
       geom_bar(position="dodge", stat="identity") + #color by region
       labs(x = "District", 
-           title = paste(names(area_options[which(area_options == input$y)]), "in Kostroma province, 1908"),
+           title = "Forested area in Kostroma province, 1908",
            y = names(area_options[which(area_options == input$y)])) +
       coord_flip()
   })}
