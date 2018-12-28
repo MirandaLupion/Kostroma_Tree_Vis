f03accde5729dd2e8561cc1b03691e251fafe828
Miranda Lupion <lupion@g.harvard.edu>
2018-12-28 18:29:32 -0500

    app in progress

--- kostroma_area_app/area_app.R
+++ kostroma_area_app/area_app.R
@@ -4,10 +4,10 @@ library(shiny)
 library(readr)
 library(stringr)
 library(shinythemes)
-# library(DT)
-# library(ggrepel)
+library(DT)
 library(plotly)
 library(tidyverse)
+library(scales)
 
 # Load data from CSV files
 
@@ -15,21 +15,24 @@ area_by_district <- read_csv("area_by_district_1908.csv") %>%
   filter(district != "All") %>%
   gather(key = "condition", value, -district)
 
+# The UI
 
-ui <- fluidPage(fluidPage(theme = shinytheme("united")),
+ui <- fluidPage(fluidPage(theme = shinytheme("flatly")),
                 
  # Application title
                 
- titlePanel("Visualizing Forests in Kostroma Province"),
-                
+ titlePanel("Forested Area in Kostroma Province"),
+
+ # Allow user to select raw or percent data form
+                 
  sidebarLayout(
    sidebarPanel(
-     
      radioButtons("radio", 
                   label = "View data as",
                   choices = list("percent" = 1, "raw" = 2), 
                   selected = 1),
-
+  
+  # Allow user to select districts to include
      
      selectizeInput(inputId = "d", #internal label
                     label = "Select districts to include", #label that user sees
@@ -38,18 +41,30 @@ ui <- fluidPage(fluidPage(theme = shinytheme("united")),
                     multiple = TRUE, # can choose multiple 
                     options = list(maxItems = 13))), #can choose up to five
    
-   # Show a plot of the generated distribution
+   # Create a tabset structure with two tabs
+  
    mainPanel(
-     plotlyOutput("plot")
-     # br(),
-     # br(),
-     # DT::dataTableOutput("table")
-   )
- )
-)
+     tabsetPanel(
+     
+    # Tab for the graph with plotly vis  
+        
+     tabPanel("Graph", 
+              h5("Mouse over the bars of the graph for more information."),
+              br(),
+              br(),
+              plotlyOutput("plot")),
+    
+     # Tab for the table
+     
+     tabPanel("Table", 
+              DT::dataTableOutput("table"))
+))))
 
-# Define server logic required to draw a histogram
+# Define server
+
 server <- function(input, output) {
+  
+  # Filter the data based on the user inputs 
   
   pop_subset <- reactive({
     req(input$d, input$radio)
@@ -67,6 +82,8 @@ server <- function(input, output) {
     }
     })
   
+  # Reactive y axis title 
+  
   y_value <- reactive({
     if (input$radio == 1) {
       y_value = "Percent"
@@ -75,6 +92,8 @@ server <- function(input, output) {
     }
   })
    
+  # Reactive plot
+  
   output$plot <- renderPlotly({
     ggplotly(tooltip = "y", ggplot(data = pop_subset(), aes_string(x = "district", y = "value", fill = "condition")) + 
       geom_bar(position="dodge", stat="identity") + #color by region
@@ -82,46 +101,38 @@ server <- function(input, output) {
            title = "Forested area in Kostroma province, 1908",
            y = y_value()) +
       coord_flip() +
+      scale_y_continuous(labels = scales::comma) +
       theme(legend.title = element_blank())) %>% 
       config(displayModeBar = FALSE)  %>%
       layout(legend = list(x = 100, y = 0.5))
-    # +
-    #   geom_text_repel(mapping = aes(label = value, fontface = "bold"),
-    #                   hjust = 1,
-                      # segment.size = 0.2)
-    
    
                 
   })
   
-  # output$table <- DT::renderDataTable({
-  #   if (input$radio == 1) {
-  #     table_data <- area_by_district %>%
-  #       filter(district %in% input$d & condition %in% c("percent_forested_suitable", "percent_forested_total")) %>%
-  #       mutate(condition = case_when(condition == "percent_forested_suitable" ~ "Percent forested (suitable land)",
-  #                                    condition == "percent_forested_total" ~ "Percent forested (total land)")) %>%
-  #       spread(condition, value)
-  #       
-  #   } else {
-  #     table_data <- area_by_district %>%
-  #       filter(district %in% input$d & condition %in% c("total_area_desyatin", "suitable_area_desyatin", "forested_area_desyatin")) %>%
-  #       mutate(condition = case_when(condition == "total_area_desyatin" ~ "Total area", 
-  #                                    condition == "suitable_area_desyatin" ~ "Suitable area",
-  #                                    condition == "forested_area_desyatin" ~ "Forested area")) %>%
-  #       spread(condition, value)
-  #     
-  #   }
-  #   
-  #   
-  #   DT::datatable(table_data, 
-  #                  rownames = FALSE
-  #                  #colnames = c()
-  #                 
-  #                 )
-  #   
-  #       
-  #   
-  # })
+  # Reactive table
+  
+  output$table <- DT::renderDataTable({
+    if (input$radio == 1) {
+      table_data <- area_by_district %>%
+        filter(district %in% input$d & condition %in% c("percent_forested_suitable", "percent_forested_total")) %>%
+        mutate(condition = case_when(condition == "percent_forested_suitable" ~ "Percent forested (suitable land)",
+                                     condition == "percent_forested_total" ~ "Percent forested (total land)")) %>%
+        spread(condition, value) %>%
+        rename(District = district)
+
+    } else {
+      table_data <- area_by_district %>%
+        filter(district %in% input$d & condition %in% c("total_area_desyatin", "suitable_area_desyatin", "forested_area_desyatin")) %>%
+        mutate(condition = case_when(condition == "total_area_desyatin" ~ "Total area",
+                                     condition == "suitable_area_desyatin" ~ "Suitable area",
+                                     condition == "forested_area_desyatin" ~ "Forested area")) %>%
+        spread(condition, value) %>%
+        rename(District = district)
+
+    }
+DT::datatable(table_data,
+                   rownames = FALSE)
+  })
   
   }
 
