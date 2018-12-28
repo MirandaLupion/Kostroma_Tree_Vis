292af59d598c00fa1b903ed1d8f093e8760c6bd0
Miranda Lupion <lupion@g.harvard.edu>
2018-12-28 11:31:38 -0500

    case_when

--- kostroma_area_app/area_app.R
+++ kostroma_area_app/area_app.R
@@ -1,7 +1,6 @@
 # Load libraries
 
 library(shiny)
-library(plotly)
 library(readr)
 library(stringr)
 library(shinythemes)
@@ -13,11 +12,11 @@ area_by_district <- read_csv("area_by_district_1908.csv") %>%
   filter(district != "All") %>%
   gather(key = "condition", value, -district)
 
-area_options = c("Total area" = "total_area_desyatin",
-                 "Suitable area" = "suitable_area_desyatin",
-                 "Forested area" = "forested_area_desyatin",
-                 "Percent forested area (of total land)" = "percent_forested_total",
-                 "Percent forested area (of suitable land)" = "percent_forested_suitable")
+# area_options = c("Total area" = "total_area_desyatin",
+#                  "Suitable area" = "suitable_area_desyatin",
+#                  "Forested area" = "forested_area_desyatin",
+#                  "Percent forested area (of total land)" = "percent_forested_total",
+#                  "Percent forested area (of suitable land)" = "percent_forested_suitable")
 
 
 ui <- fluidPage(fluidPage(theme = shinytheme("united")),
@@ -33,17 +32,12 @@ ui <- fluidPage(fluidPage(theme = shinytheme("united")),
                   label = "View data as",
                   choices = list("percent" = 1, "raw" = 2), 
                   selected = 1),
-     
-     # selectizeInput(inputId = "y", #internal label 
-     #             label = "Select measure to add to the bar graph", #label that user sees
-     #             choices = area_options, #vector of choices for user to pick from 
-     #             multiple = TRUE,
-     #             selected = area_options[1],
-     #             options = list(maxItems = 5)),
+
      
      selectizeInput(inputId = "d", #internal label
                     label = "Select districts to include", #label that user sees
                     choices = c(area_by_district$district), #choose from this list 
+                    selected = c(area_by_district$district),
                     multiple = TRUE, # can choose multiple 
                     options = list(maxItems = 13))), #can choose up to five
    
@@ -60,19 +54,32 @@ server <- function(input, output) {
   pop_subset <- reactive({
     req(input$d, input$radio)
     if (input$radio == 1) {
-      filter(area_by_district, district %in% input$d & condition %in% c("percent_forested_suitable", "percent_forested_total"))
+      area_by_district %>%
+      filter(district %in% input$d & condition %in% c("percent_forested_suitable", "percent_forested_total")) %>%
+      mutate(condition = case_when(condition == "percent_forested_suitable" ~ "Percent forested area (of suitable land)",
+                                   condition == "percent_forested_total" ~ "Percent forested area (of total land)"))
     } else {
       filter(area_by_district, district %in% input$d & condition %in% c("total_area_desyatin", "suitable_area_desyatin", "forested_area_desyatin"))
     }
     })
+  
+  
+  y_value <- reactive({
+    if (input$radio == 1) {
+      y_value = "Percent"
+    } else {
+      y_value = "Area in desyatins (approximately 2.7 acres)"
+    }
+  })
   
   output$plot <- renderPlot({
     ggplot(data = pop_subset(), aes_string(x = "district", y = "value", fill = "condition")) + 
       geom_bar(position="dodge", stat="identity") + #color by region
       labs(x = "District", 
            title = "Forested area in Kostroma province, 1908",
-           y = names(area_options[which(area_options == input$y)])) +
-      coord_flip()
+           y = y_value()) +
+      coord_flip() +
+      theme(legend.title = element_blank())
   })}
 
 # Run the application 
