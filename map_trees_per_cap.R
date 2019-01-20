7fee9a5612ecc9b2848b6aa6d1861f04287d5378
Miranda Lupion <lupion@g.harvard.edu>
2019-01-20 16:30:11 -0500

    added map

---
+++ map_trees_per_cap.R
@@ -0,0 +1,43 @@
+library(rsconnect)
+library(leaflet)
+library(rgdal)
+library(tidyverse)
+library(readr)
+
+# Import the CSV in
+# Add an OBJECTID to merge with shapefile by 
+
+per_cap <- read_csv("trees_per_cap_1848.csv") %>%
+  mutate(OBJECTID = "")
+
+per_cap$OBJECTID[1] <- 5
+per_cap$OBJECTID[2] <- 8
+per_cap$OBJECTID[3] <- 14
+per_cap$OBJECTID[4] <- 13
+per_cap$OBJECTID[5] <- 1
+per_cap$OBJECTID[6] <- 2
+per_cap$OBJECTID[7] <- 6
+per_cap$OBJECTID[8] <- 4
+per_cap$OBJECTID[9] <- 10
+per_cap$OBJECTID[10] <- 7 
+per_cap$OBJECTID[11] <- 12
+per_cap$OBJECTID[12] <- 11
+  
+  
+# Read in the shape file for the map from the files stored in the app folder
+# Project the shape file in the standard WGS 84 projection. 
+# I chose this projection because it is used by most GPS navigation tools and the tech, on which Leaflet is based
+
+kos_map <- readOGR(dsn = "kos_shp", layer = "kos_shape_georef")
+kos_map <- spTransform(kos_map, CRS("+init=epsg:4326"))
+kos_map_per_cap <- merge(kos_map, per_cap, by = "OBJECTID", duplicateGeoms = FALSE)
+
+kos_map_per_cap %>%
+  leaflet() %>%
+  addTiles() %>%
+  addPolygons(weight = 1,
+              color = "blue",
+              label = ~paste0(district),
+              highlight = highlightOptions(weight = 3,
+                                           color = "red",
+                                           bringToFront = TRUE))
